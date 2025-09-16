209a3bc6d62e74a3636eb95e11f9c3e80bde3732
erhangndz <erhangndz@live.com>
2025-09-16 22:00:25 +0300

    create project

---
+++ src/app/admin-components/category/category.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category',
+  standalone: false,
+  templateUrl: './category.html',
+  styleUrl: './category.css'
+})
+export class Category {
+
+title="Kategoriler";
+
+
+category= {id:1,name:'Kategori 1'}
+
+categoryList= [
+  {id:5,name:'Kategori 1'},
+  {id:7,name:'Kategori 2'},
+  {id:9,name:'Kategori 3'},
+  {id:10,name:'Kategori 4'},
+  {id:11,name:'Kategori 5'}
+]
+
+
+
+}
