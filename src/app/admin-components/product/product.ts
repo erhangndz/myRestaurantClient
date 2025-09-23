7ccac69a243a5da046e0917750226edc77043ed5
erhangndz <erhangndz@live.com>
2025-09-23 22:41:50 +0300

    updated

--- src/app/admin-components/product/product.ts
+++ src/app/admin-components/product/product.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../_services/product-service';
 import { ProductModel } from '../../_models/productModel';
+import Swal from 'sweetalert2';
+import { CategoryModel } from '../../_models/categoryModel';
+import { CategoryService } from '../../_services/category-service';
 
 @Component({
   selector: 'app-product',
@@ -10,13 +13,19 @@ import { ProductModel } from '../../_models/productModel';
 })
 export class Product implements OnInit {
 
-constructor(private productService:ProductService){
+constructor(private productService:ProductService,
+            private categoryService:CategoryService
+){
 
 }
 products:ProductModel[];
+categories:CategoryModel[];
+newProduct:ProductModel= new ProductModel();
+editProduct:ProductModel= new ProductModel();
 
 ngOnInit(): void {
   this.getAll();
+  this.getCategories();
 }
 
 
@@ -26,6 +35,90 @@ getAll(){
     error: err => console.log(err)
   })
 };
+
+getCategories(){
+  this.categoryService.getAll().subscribe({
+    next: values => this.categories= values,
+    error: err=> console.log(err)
+  })
+};
+
+create(){
+  this.productService.create(this.newProduct).subscribe({
+    next: value => this.products.push(value),
+    error: err=>{
+      console.log(err);
+
+      Swal.fire({
+        title:'Ürün eklenirken bir hata oluştu',
+        icon:'error',
+        timer:1500
+      })
+
+    } ,
+    complete: () => Swal.fire({
+      title:'Yeni Ürün Eklendi',
+      icon:'success',
+      timer: 1500
+    })
+  })
+};
+
+
+update(){
+  this.productService.update(this.editProduct.id,this.editProduct).subscribe({
+    error: err =>{
+      console.log(err);
+      Swal.fire({
+        title:'Ürün güncellenirken bir hata oluştu',
+        icon:'error',
+        timer:1500
+      })
+    },
+    complete: () => Swal.fire({
+      title:'Ürün başarıyla güncellendi',
+      icon:'success',
+      timer:1500
+    })
+  })
+};
+
+
+delete(id:number){
+
+  Swal.fire({
+    title:'Silmek istediğinize emin misiniz?',
+    text: 'Bu işlemi geri alamazsınız',
+    icon: 'question',
+    confirmButtonText:'Evet, Sil',
+    cancelButtonText: 'İptal',
+    showCancelButton: true
+
+  }).then(result=>{
+    if(result.isConfirmed){
+      this.productService.delete(id).subscribe({
+        error: err=> console.log(err),
+        complete: ()=>  {Swal.fire({
+        title:'Ürün başarıyla silindi',
+        icon:'success',
+        timer:1500
+      });
+    this.getAll()
+    }
+      })
+    }
+  })
+
+
+
+
+}
+
+onSelected(model:ProductModel){
+this.editProduct=model;
+}
+
+
 
 
 
